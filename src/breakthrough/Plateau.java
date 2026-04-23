2cdedde9f8932cd4ddc08255be5d7b0e7ac4296b
SaThomy57 <148277208+SaThomy57@users.noreply.github.com>
2026-04-23 09:54:51 +0200

    Restructuration de l'indentation du code ainsi que l'ajout d'une edition

--- src/breakthrough/Plateau.java
+++ src/breakthrough/Plateau.java
@@ -1,9 +1,12 @@
-package breakthrough;/*
+package breakthrough;
+/*
 Plateau.java
 Classe décrivant un plateau de jeu de Breakthrough.
 Auteur: Rémi PALLEN.
 Date de la dernière version: 03/02/2026.
- */
+
+Editeur : Bryan MOSER
+*/
 
 public class Plateau {
     private int largeur;
@@ -13,49 +16,49 @@ public class Plateau {
     public Case[][] getPlateau() {return this.plateau;}
 
     public Plateau(int largeur) {
-	this.largeur=largeur;
-	this.plateau=new Case[8][largeur];
-	for (int i=0;i<8;i++) {
-	    for (int j=0;j<largeur;j++) {
-		plateau[i][j]=new Case();
-		if (i<2) {
-		    plateau[i][j].remplace(Joueur.joueurBlanc);
-		} else if (i>5) {
-		    plateau[i][j].remplace(Joueur.joueurNoir);
+		this.largeur=largeur;
+		this.plateau=new Case[8][largeur];
+		for (int i=0;i<8;i++) {
+			for (int j=0;j<largeur;j++) {
+			plateau[i][j]=new Case();
+			if (i<2) {
+				plateau[i][j].remplace(Joueur.joueurBlanc);
+			} else if (i>5) {
+				plateau[i][j].remplace(Joueur.joueurNoir);
+			}
+			}
 		}
-	    }
-	}
     }
 
     public Joueur getCase(int i, int j) {
-	Case c=getPlateau()[i][j];
-	if (c.getOccupe()) {
-	    return c.getPion();
-	} else {
-	    return null;
-	}
+		Case c=getPlateau()[i][j];
+		if (c.getOccupe()) {
+			return c.getPion();
+		} else {
+			return null;
+		}
     }
 
     public void modifie(int x, int y, Joueur j) {/*Met le pion du joueur j dans la case x y. Si j=null, vide la case.*/
-	if (j==null) {
-	    getPlateau()[x][y].libere();
-	} else {
-	    getPlateau()[x][y].remplace(j);
-	}
+		if (j==null) {
+			getPlateau()[x][y].libere();
+		} else {
+			getPlateau()[x][y].remplace(j);
+		}
     }
 
     public void afficher() {/*Essayez d'améliorer cette méthode!*/
-	String s=" ";
-        for (int k=1;k<=this.largeur;k++) {
-	    s+=" "+k;
-	}
-	System.out.println(s);
-	for (int i=0;i<8;i++) {
-	    System.out.print(i+1);
-	    for (int j=0;j<this.largeur;j++) {
-		System.out.print("|"+getPlateau()[i][j].toString());
-	    }
-	    System.out.println("|");
-	}
+		String s=" ";
+			for (int k=1;k<=this.largeur;k++) {
+			s+=" "+k;
+		}
+		System.out.println(s);
+		for (int i=0;i<8;i++) {
+			System.out.print(i+1);
+			for (int j=0;j<this.largeur;j++) {
+			System.out.print("|"+getPlateau()[i][j].toString());
+			}
+			System.out.println("|");
+		}
     }
 }
