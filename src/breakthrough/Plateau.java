108ac0812270bd1dd2918f03da80936469e4e4f4
SaThomy57 <148277208+SaThomy57@users.noreply.github.com>
2026-04-28 00:12:29 +0200

    Ajout d'un constructeur par copie dans `Plateau` pour permettre la duplication de l'état du plateau, utile pour l'IA

--- src/breakthrough/Plateau.java
+++ src/breakthrough/Plateau.java
@@ -1,61 +1,82 @@
-package breakthrough;/*
+package breakthrough;
+/*
 Plateau.java
 Classe décrivant un plateau de jeu de Breakthrough.
 Auteur: Rémi PALLEN.
 Date de la dernière version: 03/02/2026.
- */
+
+Editeur : Bryan MOSER
+*/
 
 public class Plateau {
-    private int largeur;
-    private Case[][] plateau;
+	private int largeur;
+	private Case[][] plateau;
 
-    public int getLargeur() {return this.largeur;}
-    public Case[][] getPlateau() {return this.plateau;}
+	public int getLargeur() {return this.largeur;}
+	public Case[][] getPlateau() {return this.plateau;}
 
-    public Plateau(int largeur) {
-	this.largeur=largeur;
-	this.plateau=new Case[8][largeur];
-	for (int i=0;i<8;i++) {
-	    for (int j=0;j<largeur;j++) {
-		plateau[i][j]=new Case();
-		if (i<2) {
-		    plateau[i][j].remplace(Joueur.joueurBlanc);
-		} else if (i>5) {
-		    plateau[i][j].remplace(Joueur.joueurNoir);
+	public Plateau(int largeur) {
+		this.largeur=largeur;
+		this.plateau=new Case[8][largeur];
+		for (int i=0;i<8;i++) {
+			for (int j=0;j<largeur;j++) {
+				plateau[i][j]=new Case();
+				if (i<2) {
+					plateau[i][j].remplace(Joueur.joueurBlanc);
+				} else if (i>5) {
+					plateau[i][j].remplace(Joueur.joueurNoir);
+				}
+			}
 		}
-	    }
 	}
-    }
 
-    public Joueur getCase(int i, int j) {
-	Case c=getPlateau()[i][j];
-	if (c.getOccupe()) {
-	    return c.getPion();
-	} else {
-	    return null;
+	/**
+	 *	Constructeur par copie pour l'IA
+	 */
+	public Plateau(Plateau copie){
+		this.largeur=copie.getLargeur();
+		this.plateau = new Case[8][largeur];
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < largeur; j++) {
+				this.plateau[i][j] = new Case();
+				Joueur jCopie = copie.getCase(i, j);
+				if (jCopie != null) {
+					plateau[i][j].remplace(jCopie);
+				}
+			}
+		}
 	}
-    }
 
-    public void modifie(int x, int y, Joueur j) {/*Met le pion du joueur j dans la case x y. Si j=null, vide la case.*/
-	if (j==null) {
-	    getPlateau()[x][y].libere();
-	} else {
-	    getPlateau()[x][y].remplace(j);
+	public Joueur getCase(int i, int j) {
+		Case c=getPlateau()[i][j];
+		if (c.getOccupe()) {
+			return c.getPion();
+		} else {
+			return null;
+		}
 	}
-    }
 
-    public void afficher() {/*Essayez d'améliorer cette méthode!*/
-	String s=" ";
-        for (int k=1;k<=this.largeur;k++) {
-	    s+=" "+k;
+	public void modifie(int x, int y, Joueur j) {
+		/*Met le pion du joueur j dans la case x y. Si j=null, vide la case.*/
+		if (j==null) {
+			getPlateau()[x][y].libere();
+		} else {
+			getPlateau()[x][y].remplace(j);
+		}
 	}
-	System.out.println(s);
-	for (int i=0;i<8;i++) {
-	    System.out.print(i+1);
-	    for (int j=0;j<this.largeur;j++) {
-		System.out.print("|"+getPlateau()[i][j].toString());
-	    }
-	    System.out.println("|");
+
+	public void afficher() {/*Essayez d'améliorer cette méthode!*/
+		String s=" ";
+		for (int k=1;k<=this.largeur;k++) {
+			s+=" "+k;
+		}
+		System.out.println(s);
+		for (int i=0;i<8;i++) {
+			System.out.print(i+1);
+			for (int j=0;j<this.largeur;j++) {
+				System.out.print("|"+getPlateau()[i][j].toString());
+			}
+			System.out.println("|");
+		}
 	}
-    }
 }
